dd155140099dc0618960e99b04d14260a0256d68
hitesh-reddy-k <152591841+hitesh-reddy-k@users.noreply.github.com>
2025-01-19 12:39:07 +0530

    Update index.js-1

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow these methods
     allowedHeaders: [
         'X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version',
-        'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'
+        'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version','Authorization'
     ]
 };
 
