8fa68513d260a68f60883ef226a18482396914cd
hitesh-reddy-k <152591841+hitesh-reddy-k@users.noreply.github.com>
2025-01-22 23:19:34 +0530

    solving the error of the crating club

--- backend/routes/clubrouter.js
+++ backend/routes/clubrouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { joinClub, leaveClub } = require("../controllers/clubs");
+const { joinClub, leaveClub,createClub } = require("../controllers/clubs");
 
 router.post("/join", joinClub);
 router.post("/leave", leaveClub);
